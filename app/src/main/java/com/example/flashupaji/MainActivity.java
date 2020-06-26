3f1baef1a5992f620cee7e2a5d97b1dcbac3a743
sahildholpuria <dholpuria1999@gmail.com>
2020-06-26 21:48:44 +0530

    ok

---
+++ app/src/main/java/com/example/flashupaji/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.flashupaji;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivity extends AppCompatActivity {
+
+    Button btnFlashLight;
+    private static final int CAMERA_REQUEST = 123;
+    boolean hasCameraFlash = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST);
+
+        hasCameraFlash = getPackageManager().
+                hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        btnFlashLight = findViewById(R.id.btnFlashLightToggle);
+//        btnBlinkFlashLight = findViewById(R.id.btnBlinkFlashLight);
+
+        btnFlashLight.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                if (hasCameraFlash) {
+                    if (btnFlashLight.getText().toString().contains("ON")) {
+                        btnFlashLight.setText("FLASHLIGHT OFF");
+                        flashLightOff();
+                    } else {
+                        btnFlashLight.setText("FLASHLIGHT ON");
+                        flashLightOn();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "No flash available on your device",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void flashLightOn() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, true);
+        } catch (CameraAccessException e) {
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void flashLightOff() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, false);
+        } catch (CameraAccessException e) {
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void blinkFlash()
+    {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String myString = "0101010101";
+        long blinkDelay = 50; //Delay in ms
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) == '0') {
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        cameraManager.setTorchMode(cameraId, true);
+                    }
+                } catch (CameraAccessException e) {
+                }
+            } else {
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        cameraManager.setTorchMode(cameraId, false);
+                    }
+                } catch (CameraAccessException e) {
+                }
+            }
+            try {
+                Thread.sleep(blinkDelay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasCameraFlash = getPackageManager().
+                            hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+                } else {
+                    btnFlashLight.setEnabled(false);
+                    Toast.makeText(MainActivity.this, "Permission Denied for the Camera", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+}
